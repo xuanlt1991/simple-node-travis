e9050724773e488c703d4a52550a979a844648e3
Justin Lee <hello@leejustin.com>
2020-04-10 18:28:21 -0700

    Update environment variable logic

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const FAVORITE_INSTRUCTOR = process.env.NAME;
-
+const ANIMAL = process.env.NAME;
+  
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
+    console.log(`I really like ${ANIMAL}`);
     await sleep(5000);
   }
 }
